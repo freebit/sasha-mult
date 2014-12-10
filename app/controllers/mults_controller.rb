f98936d2daf334d911e1f0306a26f4d5cfdc40d6
Freebit <freebit77@inbox.com>
2014-12-10 10:53:52 +0300

    smart authorization create

--- app/controllers/mults_controller.rb
+++ app/controllers/mults_controller.rb
@@ -1,8 +1,29 @@
 class MultsController < ApplicationController
+
+  before_action :signed_in_user, only: [:search]
+  #before_action :correct_user
+
   def index
     @mults = Mult.all
   end
 
   def search
   end
+
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Пожалуйста авторизуйтесь!" unless signed_in?
+      end
+
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
