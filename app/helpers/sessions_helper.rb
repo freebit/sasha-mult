42e4fac55a4c737d904f9f9c91a250cc65db9511
Freebit <freebit77@inbox.com>
2014-12-12 17:09:14 +0300

    fix bug in session_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,7 @@ module SessionsHelper
   end
 
   def sign_out
+
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
@@ -21,7 +22,7 @@ module SessionsHelper
 
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    @current_user ||= User.find_by_remember_token(remember_token)
   end
 
   def signed_in?
